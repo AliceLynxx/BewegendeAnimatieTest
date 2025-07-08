ef0bb4bf30ae96ad1052b9a0c6fdcd2612916804
AliceLynxx <167111971+AliceLynxx@users.noreply.github.com>
2025-07-08 21:43:15 +0200

    Update constants.py met alleen benodigde parameters

--- constants.py
+++ constants.py
@@ -1,16 +1,15 @@
 # Configuratie voor de bewegende animatie
 
 # Ovaal afmetingen en positie
-OVAL_CENTER_X = 400  # X-coördinaat van het centrum van het ovaal
-OVAL_CENTER_Y = 300  # Y-coördinaat van het centrum van het ovaal
-OVAL_WIDTH = 200     # Breedte van het ovaal
-OVAL_HEIGHT = 150    # Hoogte van het ovaal
+OVAL_CENTER = (400, 300)  # X,Y coördinaat van het centrum van het ovaal
+OVAL_RADIUS_X = 100       # Horizontale radius van het ovaal
+OVAL_RADIUS_Y = 75        # Verticale radius van het ovaal
 
 # Animatie instellingen
 ANIMATION_FRAMES = 3      # Aantal frames in de animatie
 FRAME_DURATION = 500      # Duur per frame in milliseconden
-ANIMATION_SPEED = 2       # Snelheid van beweging binnen het ovaal
+ANIMATION_SIZE = 30       # Grootte van de animatie element
 
-# Output instellingen
-OUTPUT_FILENAME = "brain_animation.gif"
+# Bestandspaden
 BACKGROUND_PATH = "background/brain_background.png"
+OUTPUT_PATH = "brain_animation.gif"
