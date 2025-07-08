dbbaad4b34116de61d2bf5a12ae2c9b83a789f9d
AliceLynxx <167111971+AliceLynxx@users.noreply.github.com>
2025-07-08 21:27:56 +0200

    Add constants.py with configuration for animation

---
+++ constants.py
@@ -0,0 +1,16 @@
+# Configuratie voor de bewegende animatie
+
+# Ovaal afmetingen en positie
+OVAL_CENTER_X = 400  # X-coördinaat van het centrum van het ovaal
+OVAL_CENTER_Y = 300  # Y-coördinaat van het centrum van het ovaal
+OVAL_WIDTH = 200     # Breedte van het ovaal
+OVAL_HEIGHT = 150    # Hoogte van het ovaal
+
+# Animatie instellingen
+ANIMATION_FRAMES = 3      # Aantal frames in de animatie
+FRAME_DURATION = 500      # Duur per frame in milliseconden
+ANIMATION_SPEED = 2       # Snelheid van beweging binnen het ovaal
+
+# Output instellingen
+OUTPUT_FILENAME = "brain_animation.gif"
+BACKGROUND_PATH = "background/brain_background.png"
